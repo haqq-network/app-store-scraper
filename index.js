5c7821dd113f8dd1e01fe141ea5230bcd09bfb69
Jihad Hadj Husein <skiptomylou999@gmail.com>
2024-04-28 19:19:11 +0300

    fix: convert to ES module

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const R = require('ramda');
-const memoizee = require('memoizee');
-const constants = require('./lib/constants');
+import { map } from 'ramda';
+import memoizee from 'memoizee';
+import constants from './lib/constants';
 
 const methods = {
   app: require('./lib/app'),
@@ -13,18 +13,21 @@ const methods = {
   suggest: require('./lib/suggest'),
   similar: require('./lib/similar'),
   reviews: require('./lib/reviews'),
-  ratings: require('./lib/ratings')
+  ratings: require('./lib/ratings'),
 };
 
-function memoized (opts) {
-  const cacheOpts = Object.assign({
-    primitive: true,
-    normalizer: JSON.stringify,
-    maxAge: 1000 * 60 * 5, // cache for 5 minutes
-    max: 1000 // save up to 1k results to avoid memory issues
-  }, opts);
+function memoized(opts) {
+  const cacheOpts = Object.assign(
+    {
+      primitive: true,
+      normalizer: JSON.stringify,
+      maxAge: 1000 * 60 * 5, // cache for 5 minutes
+      max: 1000, // save up to 1k results to avoid memory issues
+    },
+    opts,
+  );
   const doMemoize = (fn) => memoizee(fn, cacheOpts);
-  return Object.assign({}, constants, R.map(doMemoize, methods));
+  return Object.assign({}, constants, map(doMemoize, methods));
 }
 
-module.exports = Object.assign({memoized}, constants, methods);
+export default Object.assign({ memoized }, constants, methods);
