db8488d813a2f16a4c2200d97ac5963ae9715aee
Oleg Shilov <me@olegshilov.com>
2024-04-30 21:29:35 +0700

    ci: add manual releases

---
+++ release.config.mjs
@@ -0,0 +1,23 @@
+export default {
+  branches: ['master'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: true,
+        tarballDir: 'dist',
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'CHANGELOG.md'],
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
+};
