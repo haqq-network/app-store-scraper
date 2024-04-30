3f21d82e1ce568270744b55a1d78084336c624cb
Oleg Shilov <me@olegshilov.com>
2024-04-30 21:34:50 +0700

    chore: use default semantic-release/npm config and  update release commit message

--- release.config.mjs
+++ release.config.mjs
@@ -4,19 +4,13 @@ export default {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
-    [
-      '@semantic-release/npm',
-      {
-        npmPublish: true,
-        tarballDir: 'dist',
-      },
-    ],
+    ['@semantic-release/npm'],
     [
       '@semantic-release/git',
       {
         assets: ['package.json', 'CHANGELOG.md'],
         message:
-          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+          'chore(release): ${nextRelease.version}\n\n${nextRelease.notes}\n\n[skip ci]',
       },
     ],
   ],
