33794415ad6299f67b65a88a09b92e084a0a1215
Evgeniy Abramov <i@kio.ninja>
2024-04-30 20:23:27 +0400

    feat: add publish

--- test.ts
+++ test.ts
@@ -1,9 +1,12 @@
+import { assert } from 'console';
 import store from './dist/main';
 
 async function test() {
   const app = await store.app({ id: '6443843352' });
 
   console.log({ app });
+
+  assert(app.id === 6443843352, 'App ID is not correct');
 }
 
 test();
