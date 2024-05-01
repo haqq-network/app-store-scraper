4e5a9b212d7292755cf53e294bf95e962784a515
Oleg Shilov <me@olegshilov.com>
2024-05-01 14:38:39 +0700

    chore: add missing dep, format code

--- release.config.mjs
+++ release.config.mjs
@@ -2,13 +2,11 @@
 export default {
   branches: ['master'],
   plugins: [
+    ['@semantic-release/commit-analyzer', { preset: 'conventionalcommits' }],
     [
-      '@semantic-release/commit-analyzer',
-      {
-        preset: 'conventionalcommits',
-      },
+      '@semantic-release/release-notes-generator',
+      { preset: 'conventionalcommits' },
     ],
-    '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
     ['@semantic-release/npm'],
     [
